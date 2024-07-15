9b51fe827469f809ff0ecc82af2bffc29ef0e340
sburakozdemir <sburakozdemir00@gmail.com>
2024-07-15 18:57:21 +0300

    Add project files.

---
+++ Pratik - İlk Uygulamam/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class program
+
+    {
+        static void Main(string[] args)
+
+        {
+            string metin = "Burak Özdemir" + Environment.NewLine + "Patika'nın eğitimleri ve uzman kadrosuyla yazılım sektörü gerekliliklerine ulaşmak için bu bootcampteyim. ";
+            Console.WriteLine(metin);
+        }
+    }
+}
